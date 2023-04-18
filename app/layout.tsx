113eb67dd9dfad7d7f0774d81e54866a86fa7b67
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-04-18 10:28:02 -0700

    started auth ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
