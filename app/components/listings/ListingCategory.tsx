c571c763db05c31bca109fba67eb8373749ef839
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-05-29 21:27:29 -0700

    started individual listing pages

---
+++ app/components/listings/ListingCategory.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface ListingCategoryProps {
+  icon: IconType;
+  label: string;
+  description: string;
+}
+
+const ListingCategory = () => {
+  return <div> Listing Category</div>;
+};
+
+export default ListingCategory;
