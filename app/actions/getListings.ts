46c961b516e1e4f46213be3db6bdac5f2fbd2a37
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-06-16 10:35:07 -0700

    getListings issue

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -86,6 +86,7 @@ export default async function getListings(params: IListingsParams) {
       ...listing,
       createdAt: listing.createdAt.toISOString(),
     }));
+
     return safeListings;
   } catch (error: any) {
     throw new Error(error);
